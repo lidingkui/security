ce600d72372ad466aa5293ec92bb2b2aae954b20
lidingkui <lidingkui168@126.com>
2020-06-12 11:07:27 +0800

    add CountdownLatchTest

--- concurrency/src/main/java/com/bingfaceshi/annoations/CountdownLatchTest.java
+++ concurrency/src/main/java/com/bingfaceshi/annoations/CountdownLatchTest.java
@@ -2,6 +2,11 @@ package com.bingfaceshi.annoations;
 
 import java.util.concurrent.CountDownLatch;
 
+/*
+*  CountDownLatch  就是一把倒计时锁
+*  顾名思义就是当所有线程都调用 .await();方法时，阻塞，然后调用 .countDown();
+* 方法时释放次数，线程的运行次数是由 CountDownLatch(线程的数量) 这个构造方法来决定的;
+* */
 public class CountdownLatchTest {
     public static void main(String[] args) {
         final CountDownLatch countDownLatch = new CountDownLatch(2);
@@ -10,31 +15,28 @@ public class CountdownLatchTest {
             @Override
             public void run() {
                 try {
-                    System.out.println("我是线程一，一秒后我进入等待状态");
+                    System.out.println(Thread.currentThread().getName()+"  我即将进入等待状态.....");
                     countDownLatch.await();
-                    Thread.sleep(1000);
-                    System.out.println("我是线程一，我等待结束，申请起飞");
+                    System.out.println(Thread.currentThread().getName()+"  我已经解除等待状态，开始运行");
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
-        },"t1").start();
-
+        },"线程1").start();
 
 
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    System.out.println("我是线程二，一秒钟后我也进入等待状态");
+                    System.out.println(Thread.currentThread().getName()+"   我即将进入等待状态.....");
                     countDownLatch.await();
-                    Thread.sleep(1000);
-                    System.out.println("我是线程二，等待结束，我申请起飞");
+                    System.out.println(Thread.currentThread().getName()+"  我已经解除等待状态，开始运行....");
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
-        },"t2").start();
+        }, "线程2").start();
 
 
         try {
@@ -43,15 +45,15 @@ public class CountdownLatchTest {
             e.printStackTrace();
         }
 
+
         new Thread(new Runnable() {
             @Override
             public void run() {
-                System.out.println("我是主线程，我现在减为1");
+                System.out.println(Thread.currentThread().getName()+" 等待的线程即将全部被唤醒.......");
                 for (int i = 0; i < 2; i++) {
                     countDownLatch.countDown();
                 }
-                System.out.println("我已经减为0了，即将激活线程一和线程二");
             }
-        },"主线程").start();
+        },"唤醒线程后").start();
     }
 }
