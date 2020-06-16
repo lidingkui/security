c7334aff1ac6359857943f9f65379631854b878a
lidingkui <lidingkui168@126.com>
2020-06-16 16:17:16 +0800

    增加webSoket功能

---
+++ cat/src/main/java/com/catservice/WebSocket/util/TimeConversionUtil.java
@@ -0,0 +1,10 @@
+package com.catservice.WebSocket.util;
+
+import java.time.LocalDateTime;
+
+public class TimeConversionUtil {
+
+    public static LocalDateTime getTimeStrNow() {
+        return LocalDateTime.now();
+    }
+}
---
+++ servlet/src/main/java/com/security/servlet/LoginServlet.java
@@ -0,0 +1,19 @@
+package com.security.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        String password = request.getParameter("password");
+
+        System.out.println("name:" + name);
+        System.out.println("password:" + password);
+    }
+}
